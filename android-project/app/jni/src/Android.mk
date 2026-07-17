b50a8c9f0aa90f57eebd18e97a028cacc1d2608e
Ayaanyx7 <ayaanyx7@gmail.com>
2026-07-17 07:46:40 +0100

    Update Android.mk

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -1,18 +1,23 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
 
-SDL_PATH := ../SDL
+# Mirrors the Makefile's PLATFORM=sdl source selection: exclude win32/ps2
+# platform code, keep everything else (shared game logic + pret_sdl backend)
+LOCAL_SRC_FILES := $(shell find $(LOCAL_PATH)/../../src -name "*.c" \
+    -not -path "*/platform/win32/*" \
+    -not -path "*/platform/ps2/*" \
+    -not -path "*/platform/sdl_psp/*")
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../include
 
-# Add your application source files here...
-LOCAL_SRC_FILES := YourSourceHere.c
+LOCAL_CFLAGS := -D PLATFORM_GBA=0 -D PLATFORM_SDL=1 -D PLATFORM_WIN32=0 \
+    -D CPU_ARCH_X86=0 -D CPU_ARCH_ARM=0 \
+    -D GAME=GAME_SA2 -D USA \
+    -D DEBUG=0 -D PORTABLE=0 -D TAS_TESTING=0 -D ENABLE_DECOMP_CREDITS=1 \
+    -Wno-parentheses-equality -Wno-unused-value
 
 LOCAL_SHARED_LIBRARIES := SDL2
-
-LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid
 
 include $(BUILD_SHARED_LIBRARY)
