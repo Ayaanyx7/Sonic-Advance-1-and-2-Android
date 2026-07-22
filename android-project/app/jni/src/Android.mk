ee357895d0669bbfe089d57b48e5af293aec9c1e
Ayaanyx7 <ayaanyx7@gmail.com>
2026-07-22 20:38:15 +0100

    Update Android.mk

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -1,19 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
+# 1. Define the Prebuilt Main Game Decomp Library
 include $(CLEAR_VARS)
-LOCAL_MODULE := sa2_game
+LOCAL_MODULE    := sa2_game
 LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../../libsa2_game.a
 include $(PREBUILT_STATIC_LIBRARY)
 
+# 2. Build the Final Native Library for Android
 include $(CLEAR_VARS)
-LOCAL_MODULE := agbsyscall
-LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../../libagbsyscall/build/android/libagbsyscall.a
-include $(PREBUILT_STATIC_LIBRARY)
+LOCAL_MODULE    := main
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := main
-LOCAL_SRC_FILES := stub.c
-LOCAL_WHOLE_STATIC_LIBRARIES := sa2_game agbsyscall
-LOCAL_SHARED_LIBRARIES := SDL2
-LOCAL_LDLIBS := -llog -landroid
+# Leave this empty. The game code library has its own entry point!
+LOCAL_SRC_FILES := 
+
+# Pull in your game logic library
+LOCAL_WHOLE_STATIC_LIBRARIES := sa2_game
+LOCAL_SHARED_LIBRARIES       := SDL2
+LOCAL_LDLIBS                 := -llog -landroid
+
 include $(BUILD_SHARED_LIBRARY)
