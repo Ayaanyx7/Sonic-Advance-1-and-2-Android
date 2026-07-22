8981f1a30cd773fd51a18c280377e25986f72ab4
Ayaanyx7 <ayaanyx7@gmail.com>
2026-07-22 21:14:34 +0100

    Update Android.mk

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -6,17 +6,20 @@ LOCAL_MODULE    := sa2_game
 LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../../libsa2_game.a
 include $(PREBUILT_STATIC_LIBRARY)
 
-# 2. Build the Final Native Library for Android
+# 2. Build the Final Native Shared Library for Android
 include $(CLEAR_VARS)
 LOCAL_MODULE    := main
-LOCAL_SRC_FILES := 
+
+# ⬇️ ADD THE AGBSYSCALL C FILE SOURCE HERE ⬇️
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../../libagbsyscall/src/agbsyscall.c
 
 LOCAL_CFLAGS    := -fPIC
 
+# Include headers for the syscall emulation layer
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../libagbsyscall/include
+
 LOCAL_WHOLE_STATIC_LIBRARIES := sa2_game
 LOCAL_SHARED_LIBRARIES       := SDL2
-
-# ⬇️ UPDATE THIS LINE TO ADD THE LINKER FLAGS ⬇️
 LOCAL_LDLIBS                 := -llog -landroid -Wl,-z,notext
 
 include $(BUILD_SHARED_LIBRARY)
