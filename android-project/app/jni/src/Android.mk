cea9b287efcd1d422a386a3fb0a5872a23584480
Ayaanyx7 <ayaanyx7@gmail.com>
2026-07-23 11:02:24 +0100

    Update Android.mk

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -1,22 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
-# 1. Define the Prebuilt Main Game Decomp Library
 include $(CLEAR_VARS)
 LOCAL_MODULE    := sa2_game
 LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../../libsa2_game.a
 include $(PREBUILT_STATIC_LIBRARY)
 
-# 2. Build the Final Native Shared Library for Android
 include $(CLEAR_VARS)
 LOCAL_MODULE    := main
 
-# Use a clean, flattened path variable mapping to prevent the parser from appending subfolders
-DECOMP_ROOT     := $(LOCAL_PATH)/../../../../
-LOCAL_SRC_FILES := $(DECOMP_ROOT)libagbsyscall/src/agbsyscall.c
+# Pure local name. The copy step above ensures it's right next to this file.
+LOCAL_SRC_FILES := agbsyscall.c
 
 LOCAL_CFLAGS    := -fPIC
-LOCAL_C_INCLUDES := $(DECOMP_ROOT)libagbsyscall/include
-
 LOCAL_WHOLE_STATIC_LIBRARIES := sa2_game
 LOCAL_SHARED_LIBRARIES       := SDL2
 LOCAL_LDLIBS                 := -llog -landroid -Wl,-z,notext
