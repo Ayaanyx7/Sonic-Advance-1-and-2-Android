cf5036106d1403d64fdff06e34b28dccf9ac9d1b
Ayaanyx7 <ayaanyx7@gmail.com>
2026-07-24 18:06:29 +0100

    Update Android.mk

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -17,7 +17,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := main
 
 # Use the exact flat local filename copied by your workflow
-LOCAL_CFLAGS    := -fPIC -fpie -fno-common -DWIDESCREEN_HACK=1
+LOCAL_CFLAGS    := -fvisibility=hidden -fPIC -fpie -fno-common -DWIDESCREEN_HACK=1
+LOCAL_ASFLAGS   := -fPIC
 
 # ⬇️ POINT DIRECTLY TO THE MAIN REPOSITORY HEADERS ⬇️
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../include \
