893039acc1406fc913e68a16283a4384cbdec979
Ayaanyx7 <ayaanyx7@gmail.com>
2026-09-02 19:31:03 +0100

    Enable STL support in Application.mk

--- android-project/app/jni/Application.mk
+++ android-project/app/jni/Application.mk
@@ -1,10 +1,3 @@
-
-# Uncomment this if you're using STL in your project
-# You can find more information here:
-# https://developer.android.com/ndk/guides/cpp-support
-# APP_STL := c++_shared
-
 APP_ABI := armeabi-v7a
-
-# Min runtime API level
 APP_PLATFORM=android-16
+APP_STL := c++_shared
