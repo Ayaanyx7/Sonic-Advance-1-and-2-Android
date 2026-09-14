9d1445104817ec3e2608ac2b8a7a39a91007fb07
Ayaanyx7 <ayaanyx7@gmail.com>
2026-09-14 16:44:12 +0100

    Refactor Android.mk for SDL_image integration

--- android-project/app/jni/src/Android.mk
+++ android-project/app/jni/src/Android.mk
@@ -1,22 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
-SDL_IMAGE_PATH := $(LOCAL_PATH)/../SDL_image
-
-# Build SDL2_image as an Android module.
-include $(SDL_IMAGE_PATH)/Android.mk
-
-
 include $(CLEAR_VARS)
 LOCAL_MODULE    := sa2_game
 LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../../libsa2_game.a
 include $(PREBUILT_STATIC_LIBRARY)
 
-
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libagbsyscall
 LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../../libagbsyscall/build/android/libagbsyscall.a
 include $(PREBUILT_STATIC_LIBRARY)
-
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := main
@@ -25,11 +17,10 @@ LOCAL_CFLAGS := -fPIC -fpie -fno-common -DWIDESCREEN_HACK=1
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../include \
                     $(LOCAL_PATH)/../../../../libagbsyscall \
-                    $(SDL_IMAGE_PATH)/include
+                    $(LOCAL_PATH)/../SDL_image/include
 
 LOCAL_WHOLE_STATIC_LIBRARIES := sa2_game agbsyscall
-
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image
+LOCAL_SHARED_LIBRARIES       := SDL2 SDL2_image
 
 LOCAL_LDLIBS := -llog -landroid \
                 -Wl,--allow-multiple-definition \
